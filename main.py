aa62463fe63ec744a0e0ce29bf400b6964d13439
Raffy27 <raffy27@pm.me>
2023-08-26 12:33:35 +0300

    Extract colors and display them in a table

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
 from clarifai_grpc.grpc.api.status import status_code_pb2
 from dotenv import load_dotenv
 
+from colors import ColorResult
+
 load_dotenv()
 
 USER_ID = 'pizzahunter2000'
@@ -21,7 +23,7 @@ stub = service_pb2_grpc.V2Stub(channel)
 metadata = (('authorization', 'Key ' + PAT),)
 userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
 
-def doTheThing(file):
+def runWorkflow(file):
     post_workflow_results_response = stub.PostWorkflowResults(
         service_pb2.PostWorkflowResultsRequest(
             user_app_id=userDataObject,  
@@ -45,11 +47,18 @@ def doTheThing(file):
     results = post_workflow_results_response.results[0]
     return results
 
-
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    results = doTheThing(uploaded_file.read())
+# Converts the results from the Clarifai Workflow to a list of ColorResult objects
+def getColors(results):
+    colorResults = []
     for output in results.outputs:
         if output.model.id == "color-recognition":
             for color in output.data.colors:
-                st.write(color)
+                colorResults.append(ColorResult(color.raw_hex, color.w3c, color.value))
+    return colorResults
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    results = runWorkflow(uploaded_file.read())
+    colors = getColors(results)
+    # Create a table of name and hex values
+    st.write(pd.DataFrame.from_records([{ 'name': color.name, 'hex': color.hex } for color in colors]))
